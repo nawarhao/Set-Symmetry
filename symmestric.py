86a542df38beba93346f9de6c80fbaa7019cf6cc
razonno <nrazonno@gmail.com>
2025-07-01 20:24:28 -0400

    Add files via upload

---
+++ symmestric.py
@@ -0,0 +1,13 @@
+# Set A
+set1_a = {'blue', 'green'}
+set2_a = {'blue', 'yellow'}
+result_a = set1_a.symmetric_difference(set2_a)
+
+# Set B
+set1_b = {1, 2, 3, 4, 5}
+set2_b = {1, 5, 6, 7, 8, 9}
+result_b = set1_b.symmetric_difference(set2_b)
+
+# Output
+print("Symmetric difference for Set A:", result_a)
+print("Symmetric difference for Set B:", result_b)
